cd3e41b820e743cb395c65bb40a0f8aeed66ab52
vahid <vahid.danesh@stonybrook.edu>
2024-11-02 20:10:50 -0400

    blink led is added

--- python/blink/main.py
+++ python/blink/main.py
@@ -1,12 +1,14 @@
-import pycom
+import machine
 import time
 
-pycom.heartbeat(False)
 
+# Set up pin 2 as an output pin
+led = machine.Pin(2, machine.Pin.OUT)
+
+# Blink the LED in a loop
 while True:
-    pycom.rgbled(0xFF0000)  # Red
-    time.sleep(1)
-    pycom.rgbled(0x00FF00)  # Green
-    time.sleep(1)
-    pycom.rgbled(0x0000FF)  # Blue
-    time.sleep(1)
+    led.on()      # Turn LED on
+    time.sleep(0.05) # Wait 1 second
+    led.off()     # Turn LED off
+    time.sleep(0.05) # Wait 1 second
+
